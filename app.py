b42120e795c0071f9590e4b37cb39b693bf6dc82
Aaron Isaacs <isaacsaaron@gmail.com>
2026-06-14 15:07:35 -0500

    More balloons

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import time
 
 
 def main():
-    if st.button("ballons for Jacob"):
+    if st.button("Ballons for Jacob"):
         st.balloons()
+
+    if st.button("Never ending balloons for Jacob [:red[DANGER]]"):
+        while True:
+            st.balloons()
+            time.sleep(1)
 
 
 if __name__ == "__main__":
